1dfaf37174b36e7db997a1f4ee161f57e1108a34
yassataiseer <56459253+yassataiseer@users.noreply.github.com>
2021-04-05 10:58:49 -0400

    Update app.py

--- DlvrMe-API/app.py
+++ DlvrMe-API/app.py
@@ -34,11 +34,21 @@ class spec_order(Resource):
     def get(self,Username):
         data = order.get_order_specific_person(Username)
         return jsonify(data)
+class find_address(Resource):
+    def get(self,Address):
+        status = order.grab_address(Address)
+        return jsonify(status)
+class validate_address(Resource):
+    def get(self,Address):
+        status = order.validate_address(Address)
+        return jsonify(status)
 api.add_resource(mk_user,"/mk_user/<string:Username>/<string:Password>")
 api.add_resource(validate_user,"/validate_user/<string:Username>/<string:Password>")
 api.add_resource(mk_order,"/mk_order/<string:Username>/<string:Address>/<string:Item>/<float:Price>/<string:User_Info>")
 api.add_resource(del_order,"/del_order/<string:Username>/<string:Address>/<string:Item>/<float:Price>/<string:User_Info>")
 api.add_resource(all_order,"/all_order")
 api.add_resource(spec_order,"/spec_order/<string:Username>")
+api.add_resource(find_address,"/find_address/<string:Address>")
+api.add_resource(validate_address,"/validate_address/<string:Address>")
 if __name__ == "__main__":
-	app.run(host='10.0.0.53', port=5000 ,debug=True)
+	app.run(host='10.0.0.63', port=5000 ,debug=True)
